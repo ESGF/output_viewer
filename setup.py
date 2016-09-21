1da2289abd7d77a84d6790cf44ffafb97614f55b
Sam Fries <fries2@llnl.gov>
2016-09-21 11:43:55 -0700

    Refactored setup version out so I can make a release script

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 import os
 
+version = "1.0.7"
+
 
 setup(
     name="output_viewer",
-    version="1.0.7",
+    version=version,
     description="Framework for building web pages to examine output.",
     author="Sam Fries",
     author_email="fries2@llnl.gov",
